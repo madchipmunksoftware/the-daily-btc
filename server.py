5d8f3891d6e0f3ba470351a743806f0135c2248c
Tam Le <tamle@Tams-MacBook-Air.local>
2024-09-25 22:30:07 -0700

    Refactor for gunicorn

--- server.py
+++ server.py
@@ -14,27 +14,27 @@ app = Flask(__name__)
 def get_home_page():
     return redirect("/home/")
 
-# Instantiate Managers With Latest Data
-database_manager = DataBaseManager()
-dashboard_manager = DashBoardManager(app)
-database_manager.update()
-dashboard_manager.update(database_manager.read())
-
-# Schedule Background Tasks
-scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(
-    func=database_manager.update, 
-    trigger='interval', 
-    seconds=database_manager.update_rate_sec
-    )
-scheduler.add_job(
-    func=dashboard_manager.update, 
-    args=[database_manager.read()],
-    trigger='interval',
-    seconds=database_manager.update_rate_sec + 60 * 10 # 10-Minute Delays For DB To Update
-    )
-scheduler.start()
-
 if __name__ == "__main__":
+    # Instantiate Managers With Latest Data
+    database_manager = DataBaseManager()
+    dashboard_manager = DashBoardManager(app)
+    database_manager.update()
+    dashboard_manager.update(database_manager.read())
+
+    # Schedule Background Tasks
+    scheduler = BackgroundScheduler(daemon=True)
+    scheduler.add_job(
+        func=database_manager.update, 
+        trigger='interval', 
+        seconds=database_manager.update_rate_sec
+        )
+    scheduler.add_job(
+        func=dashboard_manager.update, 
+        args=[database_manager.read()],
+        trigger='interval',
+        seconds=database_manager.update_rate_sec + 60 * 10 # 10-Minute Delays For DB To Update
+        )
+    scheduler.start()
+
     # Run Server
-    app.run()
+    app.run(host="0.0.0.0", port="8000")
